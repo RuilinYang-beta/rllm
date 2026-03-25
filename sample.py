cbb40e15ffb8145a40462b811d866d7f8884161e
Ruilin Yang <ruilin.yang@protonmail.com>
2026-03-25 09:27:31 +0800

    data now in csv; naive CoT working

--- sample.py
+++ sample.py
@@ -2,16 +2,31 @@ from inspect_ai import Task, task
 from inspect_ai.dataset import Sample
 from inspect_ai.scorer import exact
 from inspect_ai.solver import generate
+from inspect_ai.dataset import FieldSpec, csv_dataset
+from subset_data import SAVE_PATH, DEV_SAVE_PATH
+
+TEMPLATE_ONE_SHOT_PATH = "./prompts/template_one_shot.txt"
+
+def load_dataset(path: str, template_path: str):
+    with open(template_path, "r") as f:
+        template = f.read()
+
+    def record_to_sample(record) -> Sample:
+        input_text = template.replace("$PUZZLE$", record["puzzle"]) \
+                            .replace("$SOLUTION_INST$", record["solution_inst"])
+        return Sample(
+            id=record["id"],
+            input=input_text,
+            target=record["solution_alt"],
+            metadata={"size": record["size"]},
+        )
+
+    return csv_dataset(path, record_to_sample)
 
 @task
 def hello_world():
     return Task(
-        dataset=[
-            Sample(
-                input="Just reply with Hello World",
-                target="Hello World",
-            )
-        ],
+        dataset=load_dataset(DEV_SAVE_PATH, TEMPLATE_ONE_SHOT_PATH),
         solver=[generate()],
         scorer=exact(),
     )
--- subset_data.py
+++ subset_data.py
@@ -1,25 +1,44 @@
 import os
+import pandas as pd
+import json 
+import ast 
 from dotenv import load_dotenv
+from pprint import pprint
 from datasets import load_dataset, concatenate_datasets, load_from_disk
 
 PLACEHOLDER = "___"
-TARGET_SIZES = ["2*4", "3*4", "4*5", "5*6"]
+# every upgrade of level: 
+# search space -> 2x
+# z3 conflict -> 3x
+TARGET_SIZES = ["2*5", "3*4", "4*5", "5*6"]
 SAMPLE_PER_SIZE = 8
 SEED = 42
-SAVE_PATH = "./data/small_dataset/"
+SAVE_PATH = "./data/small_dataset.csv"
+DEV_SAVE_PATH = "./data/dev_dataset.csv"
 
-# transform solution format to stay consistent with the CoT prompt in ZebraLogic paper
-def add_solution_alt(row): 
-    s_alt = {
+# add a `solution_inst` and `solution_alt` column 
+# that complies to the format in prompting template
+# `solution_inst` is part of the prompt, and `solution_alt` should be the answer 
+def process_solution(row): 
+    s_inst = {
         "reasoning": PLACEHOLDER, 
         "solution": {}
     }
+    s_alt = {
+        "reasoning": PLACEHOLDER, 
+        "solution": {}
+    }    
     s = row['solution'] 
     for r in s['rows']: 
+        s_inst['solution'][f'House {r[0]}'] = {}
         s_alt['solution'][f'House {r[0]}'] = {}
-        for attr in s['header'][1:]: 
-            s_alt['solution'][f'House {r[0]}'][attr] = PLACEHOLDER
-
+        
+        for attr_idx in range(1, len(s['header'])):
+            attr_name = s['header'][attr_idx]
+            s_inst['solution'][f'House {r[0]}'][attr_name] = PLACEHOLDER
+            s_alt['solution'][f'House {r[0]}'][attr_name] = r[attr_idx]
+            
+    row['solution_inst'] = s_inst
     row['solution_alt'] = s_alt
     return row
 
@@ -37,7 +56,6 @@ def main():
     # load HuggingFace token `HF_READ_TOKEN` from env 
     load_dotenv()
 
-
     ds = load_dataset("allenai/ZebraLogicBench-private", "grid_mode", 
                     split='test', 
                     token=os.getenv("HF_READ_TOKEN"))
@@ -45,20 +63,43 @@ def main():
     small_dataset = sample_by_size(ds, TARGET_SIZES, SAMPLE_PER_SIZE, SEED)
 
 
-    small_dataset = small_dataset.map(add_solution_alt)
+    small_dataset = small_dataset.map(process_solution)
 
-    small_dataset.save_to_disk(SAVE_PATH)
+    dev(small_dataset)
+
+    small_dataset.to_csv(SAVE_PATH, index=False)
+
+    # small_dataset.save_to_disk(SAVE_PATH)
     print(f"Saved {len(small_dataset)} rows to {SAVE_PATH}")
 
 
-def test(): 
-    ds = load_from_disk(SAVE_PATH)
-    print(f"Loaded local dataset:\n{repr(ds)}")
+def test(dev=False):
+    if dev: 
+        path = DEV_SAVE_PATH
+    else: 
+        path = SAVE_PATH
+
+    df = pd.read_csv(path)
+
+    print(df['solution'].iloc[0])
+    
+    for col in ['solution_inst', 'solution_alt']: 
+        value = df[col].iloc[0]
+        parsed = ast.literal_eval(value)
+        parsed["solution"] = json.loads(parsed["solution"])
+        print(json.dumps(parsed, indent=2))
+
+
+def dev(ds): 
+    dev_dataset = ds.select(range(2))
+    dev_dataset.to_csv(DEV_SAVE_PATH, index=False)
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+
     # test()
+    test(dev=True)
 
 
 
